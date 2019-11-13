aa7e0b0eb23000d9e4be27070d8838436d0773e0
Ilya Lysenko <lysenko.ilya@gmail.com>
2019-11-13 03:22:29 +0300

    refactoring

--- src/main/java/ru/ilysenko/liquibasebackuper/Backuper.java
+++ src/main/java/ru/ilysenko/liquibasebackuper/Backuper.java
@@ -1,11 +1,14 @@
 package ru.ilysenko.liquibasebackuper;
 
 import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.diff.output.DiffOutputControl;
 import liquibase.diff.output.StandardObjectChangeFilter;
+import liquibase.integration.commandline.CommandLineUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -17,13 +20,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static liquibase.integration.commandline.CommandLineUtils.doGenerateChangeLog;
 
 @Slf4j
 @RequiredArgsConstructor
 public class Backuper {
     private static final String AUTHOR = "auto-backuper";
     private static final String CHANGELOG_FILE_NAME_TEMPLATE = "backup_{0}.xml";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = ofPattern("yyyyMMdd_HHmmss");
 
     private final LiquibaseBackuperDataSource dataSource;
     private final LiquibaseBackuperProperties properties;
@@ -32,20 +35,35 @@ public class Backuper {
     public void init() {
         log.info("Backing up...");
         try (Connection connection = dataSource.getConnection()) {
-            JdbcConnection jdbcConnection = new JdbcConnection(connection);
-            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
-            DiffOutputControl diffOutputControl = new DiffOutputControl(false, false, false, null);
-
-            List<String> tables = properties.getTables();
-            if (tables != null && !tables.isEmpty()) {
-                setTablesFilter(diffOutputControl, tables);
-            }
-            String changeLogFile = MessageFormat.format(CHANGELOG_FILE_NAME_TEMPLATE, LocalDateTime.now().format(ofPattern("yyyyMMdd_HHmmss")));
-            doGenerateChangeLog(changeLogFile, database, null, null, SnapshotType.DATA, AUTHOR, null, null, diffOutputControl);
+            generateChangeLog(getChangeLogFileName(), getDatabase(connection), SnapshotType.DATA, AUTHOR, makeDiffOutputControl());
         } catch (Exception e) {
             throw new RuntimeException("Error backing up data", e);
         }
         log.info("Backing up completed");
+    }
+
+    @SneakyThrows
+    private void generateChangeLog(String fileName, Database database, String snapshotTypes, String author, DiffOutputControl diffOutputControl) {
+        CommandLineUtils.doGenerateChangeLog(fileName, database, null, null, snapshotTypes, author, null, null, diffOutputControl);
+    }
+
+    private String getChangeLogFileName() {
+        return MessageFormat.format(CHANGELOG_FILE_NAME_TEMPLATE, LocalDateTime.now().format(DATE_TIME_FORMATTER));
+    }
+
+    @SneakyThrows
+    private Database getDatabase(Connection connection) {
+        DatabaseConnection databaseConnection = new JdbcConnection(connection);
+        return DatabaseFactory.getInstance().findCorrectDatabaseImplementation(databaseConnection);
+    }
+
+    private DiffOutputControl makeDiffOutputControl() {
+        DiffOutputControl diffOutputControl = new DiffOutputControl(false, false, false, null);
+        List<String> tables = properties.getTables();
+        if (tables != null && !tables.isEmpty()) {
+            setTablesFilter(diffOutputControl, tables);
+        }
+        return diffOutputControl;
     }
 
     private void setTablesFilter(DiffOutputControl diffOutputControl, List<String> tables) {
